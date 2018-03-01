f23e46863f78c2409df026c4cd2c238977ba82ad
Elton Memishaj <eltoni.memishi@live.com>
2018-03-01 17:58:16 +0100

    Department Table Done (7 more to go)

--- src/main/java/project/managedbeans/UserLoginRequest.java
+++ src/main/java/project/managedbeans/UserLoginRequest.java
@@ -1,16 +1,18 @@
 package project.managedbeans;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import project.dao.UserLoginDao;
 import project.entity.User;
 
 @SuppressWarnings("deprecation")
-@RequestScoped
+@SessionScoped
 @ManagedBean(name = "userLoginRequest")
-public class UserLoginRequest {
+public class UserLoginRequest implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String username, password;
     public String getUsername() {
         return username;
